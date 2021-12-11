b632ee3509d0fc83a6c1ad11ff616541026bfe16
mahbub.pasha <mahbub.pasha@randomaccessinc.com>
2021-12-11 11:52:34 +0600

    calendar event view

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'event_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TableCalendar Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: TableEventsExample(),
+    );
+  }
+}
+
+
